471368d7e3e3a19008a47d0889d9ed1d1f694bbc
Severyn Kozak <severyn.kozak@gmail.com>
2015-04-20 16:10:33 -0400

    Finish the prototype node implementation.
    pelias.js
    	-Finish implementing the node version of the utility, as started
    	in f6e8dd5.
    	-Functional, but rough. All of the code currently lives in the
    	global scope and should be broken up into several functions.

--- pelias.js
+++ pelias.js
@@ -1,13 +1,22 @@
 'use strict';
 
+var colors = require( 'colors' );
+var childProcess = require( 'child_process' );
+var path = require( 'path' );
+var fs = require( 'fs' );
+var util = require( 'util' );
 var args = process.argv.slice( 2 );
 
+function $( shellCmd ){
+  return childProcess.execSync( shellCmd ).toString();
+}
+
 function printErr( msg ){
-  console.error( msg );
+  console.error( 'pelias:'.blue, 'error:'.red, msg );
 }
 
 function printOut( msg ){
-  console.log( msg );
+  console.log( 'pelias:'.blue, msg );
 }
 
 var targetRepo = {};
@@ -34,6 +43,54 @@ else {
     targetRepo.name = repoBranchNames.substring( 0, poundIndex );
     targetRepo.branch = repoBranchNames.substring( poundIndex + 1 );
   }
+
+  targetRepo.subcommandArgs = args.slice( 2 );
 }
 
-console.log( JSON.stringify( targetRepo, undefined, 4 ) );
+var peliasCachePath = path.join( process.env.HOME, '.pelias/' );
+var repoPath = path.join( peliasCachePath, targetRepo.name );
+if( fs.existsSync( repoPath ) ){
+  process.chdir( repoPath );
+  if( $( 'git rev-parse --abbrev-ref HEAD' ).trim() === targetRepo.branch ){
+    var stale = $( 'git fetch --dry-run 2>&1' ).length > 0;
+    if( stale ){
+      printOut( 'Pulling latest changes.' );
+      $( 'git pull' );
+      printOut( 'Reinstalling.' );
+      $( 'npm install' );
+    }
+  }
+  else {
+    $( 'git checkout -q ' + targetRepo.branch );
+    var stale = $( 'git fetch --dry-run' ).length > 0;
+    if( stale ){
+      printOut( 'Pulling latest changes.' );
+      $( 'git pull' );
+    }
+    printOut( 'Installing.' );
+    $( 'npm install' );
+  }
+}
+else {
+  process.chdir( peliasCachePath );
+  printOut( 'Cloning repo.' );
+  $( 'git clone -q https://github.com/pelias/' + targetRepo.name );
+  process.chdir( targetRepo.name );
+  if( targetRepo.branch !== 'master' ){
+    $( 'git checkout -q ' + targetRepo.branch );
+  }
+  printOut( 'Installing.' );
+  $( 'npm install' );
+}
+
+if( targetRepo.subcommand ){
+  var proc = childProcess.spawn( 'npm', [ '-s', 'run', targetRepo.subcommand, '--' ].concat( targetRepo.subcommandArgs ) );
+  [ 'stdout', 'stderr' ].forEach( function ( outType ){
+    proc[ outType ].on( 'data', function ( data ){
+      process[ outType ].write( data.toString() );
+    });
+  });
+}
+else {
+  console.log( $( 'npm run' ) );
+}
